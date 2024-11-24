60adb4e3712df329048cf5f84c53967f7b415ed7
kahina lounis <kahinalounis2014@outlook.com>
2024-11-24 14:03:10 +0100

    Initial Commit

---
+++ SwiftUIAnimations/SwiftUIAnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAnimationsApp.swift
+//  SwiftUIAnimations
+//
+//  Created by Kahina Lounis on 24/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
