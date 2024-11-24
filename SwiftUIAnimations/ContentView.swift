28f6897d3d301c66137f55ce1c61df9f56a1f595
kahina lounis <kahinalounis2014@outlook.com>
2024-11-24 15:27:23 +0100

    first commit swiftui animations

--- SwiftUIAnimations/ContentView.swift
+++ SwiftUIAnimations/ContentView.swift
@@ -8,17 +8,56 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var isShown: Bool = false
+    
+    // MARK: - Body
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Transitions")
+                .font(.system(.title, weight: .bold))
+            if isShown {
+                imageView
+                    .transition(.slide)
+                imageView
+                    .transition(.opacity)
+                imageView
+                    .transition(.scale)
+                imageView
+                    .transition(.scale.combined(with: .slide))
+                imageView
+                    .transition(.asymmetric(insertion: .slide, removal: .scale))
+                imageView
+                    .transition(.move(edge: .top))
+            }
+            Spacer()
+            Button {
+                withAnimation {
+                    isShown.toggle()
+                }
+            } label: {
+                Text("Show")
+                    .foregroundColor(.white)
+                    .padding(.vertical, 15)
+                    .padding(.horizontal, 25)
+                    .background(Color.green)
+                    .clipShape(Capsule())
+            }
+            .padding()
         }
-        .padding()
+    }
+    
+    @ViewBuilder
+    private var imageView: some View {
+        Image(systemName: "bird.fill")
+            //.resizable()
+            .foregroundColor(.blue)
+            .scaledToFit()
+            .frame(width: 50, height: 50)
     }
 }
 
 #Preview {
     ContentView()
+        .preferredColorScheme(.dark)
 }
